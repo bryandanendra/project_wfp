18b0277a29eab112bc8e6b100cb3b775bc8c8f4d
bryandanendra <bryan4icm@gmail.com>
2025-05-04 00:35:05 +0700

    ktg

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -46,7 +46,7 @@
         <div class="info-box mb-3">
             <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-tag"></i></span>
             <div class="info-box-content">
-                <span class="info-box-text">Total kategori</span>
+                <span class="info-box-text">Total Kategori</span>
                 <span class="info-box-number">{{ $totalCategories }}</span>
             </div>
         </div>
